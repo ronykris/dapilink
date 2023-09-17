a1fc7b4d058ccf8a1c5aaae99390a534534baffe
ronykris <rony.kris@gmail.com>
2023-09-17 15:06:08 +0530

    update

--- apilink/contracts/Apilink.sol
+++ apilink/contracts/Apilink.sol
@@ -6,27 +6,36 @@ contract Apilink {
     address private user;
     bool private toInvoke;
     uint256 private callid;
-    //apicalldetails[] private apicalls;
-
+    string private password;
+    bool private isPasscodeSet;
+    address private node;
+    string private nodeName;
+    bool private loggedIn;
+    
     struct apicalldetails {        
         string endpoint;
         string method;
         string body;  
         string headers;
     }
-
+    //apicalldetails[] private jobs;
     apicalldetails api = apicalldetails("", "GET", "", "");
-    mapping (address => mapping(uint256 => apicalldetails)) private apicalls;
+    mapping (address => mapping(uint256 => apicalldetails)) private apicalls;    
+
+    mapping(address => string) private pswdMgr;
 
     constructor() {                
         toInvoke = false; 
-        callid = 0;                        
+        callid = 0;  
+        loggedIn = false;                      
     }    
 
     event invoked(
         bool invoke,
         uint256 id
     );
+
+    event passcodeSet(bool passwordset);
 
     function createApiCall(uint256 _callid, string calldata _endpoint, string calldata _method, string calldata _body, string calldata _headers) external payable {
         require(_callid > 0, "Call id cannot be null");
@@ -45,10 +54,33 @@ contract Apilink {
 
         toInvoke = false; //Reset        
         apicalls[msg.sender][_callid] = apicalldetails(_endpoint, _method, _body, _headers);
-
     }
 
     function getApiSpec(address _user, uint256 _id) external view returns (apicalldetails memory){
         return apicalls[_user][_id];
     }
+
+    function login(string calldata _passcode) external {
+        require(bytes(_passcode).length > 0, "Passcode cannot be empty");
+
+        node = msg.sender;        
+        isPasscodeSet = true;
+
+        emit passcodeSet(isPasscodeSet);
+
+        isPasscodeSet = false;
+        password =  string(abi.encodePacked(node, _passcode));
+        pswdMgr[node] = password;
+
+    }
+
+    function isLoggedIn(string calldata _code) external returns (bool) {
+        require(
+            keccak256(bytes(password)) == keccak256(bytes(string(abi.encodePacked(msg.sender, _code)))),
+            "Doesn't match"
+        );
+        loggedIn = true;
+        return loggedIn;
+    }
+
 }
