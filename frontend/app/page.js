c2d2844434875c32d20d64f73b808245c89c1747
Arsalaan-Alam <studyfreak1234@gmail.com>
2023-10-05 02:06:36 +0530

    dynamic results page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 export default function Home() {
   const [showSearch, setShowSearch] = useState(false);
   const [showConnectButton, setShowConnectButton] = useState(true);
+  const [walletAddress, setWalletAddress] = useState(null);
 
   const shinyTextStyles = {
     fontSize: '7rem',
@@ -14,17 +15,36 @@ export default function Home() {
   };
 
   const searchBarStyles = {
-    display: showSearch ? 'block' : 'none',
+    width: showSearch ? '66.67%' : '0', // 2/3 of the screen width when visible
   };
 
-  const handleConnectWalletClick = () => {
-    setShowSearch(true);
-    setShowConnectButton(false); // Hide the "Connect Wallet" button
+  const handleConnectWalletClick = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const selectedAddress = accounts[0];
+
+        if (selectedAddress) {
+          setWalletAddress(selectedAddress);
+          setShowSearch(true);
+          setShowConnectButton(false);
+          console.log(selectedAddress)
+        } else {
+          alert('No wallet address is available.');
+        }
+      } else {
+        alert('Metamask is not installed or not detected.');
+      }
+    } catch (error) {
+      console.error('Error connecting to Metamask:', error);
+    }
   };
 
   const handleLogoutClick = () => {
     setShowSearch(false);
-    setShowConnectButton(true); // Show the "Connect Wallet" button again
+    setShowConnectButton(true); 
+    setWalletAddress(null);
+    console.log("logged out")
   };
 
   return (
@@ -42,19 +62,30 @@ export default function Home() {
           Connect Wallet
         </button>
       )}
-      <div style={searchBarStyles} className="mt-4 flex items-center">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="border rounded-l py-2 px-4 outline-none"
-        />
-        <button
-          onClick={handleLogoutClick}
-          className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r"
-        >
-          Logout
-        </button>
-      </div>
+      {showSearch && (
+        <div style={searchBarStyles} className="mt-4 flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="border-2 border-black-400 rounded py-2 px-4 outline-none w-full"
+          />
+          <button
+            className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded"
+          >
+            Search
+          </button>
+        </div>
+      )}
+      {showSearch && (
+        <div className="mt-4 flex items-center">
+          <button
+            onClick={handleLogoutClick}
+            className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3"
+          >
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
