bfe4cf928af5050ffc4ea64a20f02e799903e00a
Arsalaan-Alam <studyfreak1234@gmail.com>
2023-10-05 02:18:36 +0530

    more css changes

--- frontend/app/search/page.js
+++ frontend/app/search/page.js
@@ -6,6 +6,7 @@ const DataRenderer = () => {
   const [images, setImages] = useState([]);
   const [videos, setVideos] = useState([]);
   const [organicResults, setOrganicResults] = useState([]);
+  const [relatedQuestions, setRelatedQuestions] = useState([]);
 
   useEffect(() => {
     // Fetch data from the specified endpoint
@@ -13,9 +14,12 @@ const DataRenderer = () => {
       .get('http://35.200.178.102:8080/ipfs/Qmbspr5KdwTjxiD4jQKp3Uv3wNhERjxoao2barxEyGxkGp')
       .then((response) => {
         // Extract inline_images, inline_videos, and organic_results from the response
+        console.log(response.data)
         const inlineImages = response.data.inline_images.slice(0, 6); // Display only the first 6 images
         const inlineVideos = response.data.inline_videos;
         const organicResultsData = response.data.organic_results;
+        const relatedQuestionsData = response.data.related_questions;
+        setRelatedQuestions(relatedQuestionsData);
         setImages(inlineImages);
         setVideos(inlineVideos);
         setOrganicResults(organicResultsData);
@@ -27,7 +31,7 @@ const DataRenderer = () => {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold m-4">Image Gallery</h1>
+      <h1 className="text-2xl font-bold m-4 ml-8 mt-8">Image Gallery</h1>
       <div className="flex flex-wrap">
         {images.map((image, index) => (
           <div key={index} className="w-1/8 p-4">
@@ -38,7 +42,7 @@ const DataRenderer = () => {
         ))}
       </div>
 
-      <h1 className="text-2xl font-bold mt-8 m-4">Videos</h1>
+      <h1 className="text-2xl font-bold mt-8 m-4 ml-8">Videos</h1>
       <div className="flex flex-wrap">
         {videos.map((video, index) => (
           <div key={index} className="w-1/4 p-4">
@@ -53,16 +57,28 @@ const DataRenderer = () => {
       </div>
 
       
-      <h1 className="text-2xl font-bold mt-8 m-4">Results</h1>
+      <h1 className="text-2xl font-bold mt-8 m-4 ml-8">Results</h1>
       <ul>
         {organicResults.map((result, index) => (
-          <li key={index} className='ml-4 mb-2'>
+          <li key={index} className='ml-8 mb-3 max-w-6xl'>
             <a href={result.link} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
               <h3 className="text-xl font-semibold">{result.title}</h3>
               <p className="text-gray-600">{result.displayed_link}</p>
               </a>
               <p className="text-gray-800">{result.snippet}</p>
           
+          </li>
+        ))}
+      </ul>
+
+      <h1 className="text-2xl font-bold mt-12 mb-4 ml-8">Related Questions</h1>
+      <ul className='ml-8 mb-4'>
+        {relatedQuestions.map((question, index) => (
+          <li key={index} className="mb-4 max-w-6xl">
+            <a href={question.link} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+              <h3 className="text-xl font-semibold">{question.question}</h3>
+              <p className="text-gray-800">{question.snippet}</p>
+            </a>
           </li>
         ))}
       </ul>
